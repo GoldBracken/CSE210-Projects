19599dcf1d6ec364264df2d529dd4c1f3e5bff61
Austin Brackney <brackney2019@hotmail.com>
2026-04-25 11:37:18 -0600

    Modified Prep2/Program.cs Added code that prompts user for percent grade and parses input to int. if-elseif-else statements than determine letter grade. Prints what letter grade was recieved and states whether they passed or not.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percent grade (ex: 89)? ");
+        string n = Console.ReadLine();
+        int grade = int.Parse(n);
+
+        bool passed = true;
+        string letter = "";
+
+        if (grade >= 90) 
+        {
+           letter = "A";
+        }
+        else if (grade >= 80) 
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60) 
+        {
+            letter = "D";
+            passed = false;
+        }
+        else
+        {
+            letter = "F";
+            passed = false;
+        }
+
+        if(grade%10 >= 7 && letter != "F" && letter != "A")
+        {
+            letter = letter + "+";
+        }
+        else if (grade%10 < 3 && letter != "F")
+        {
+            letter = letter + "-";
+        }
+
+        Console.WriteLine($"You got a {letter}");
+
+        if(passed)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, but you still learned! You'll do better next time.");
+        }
     }
 }
