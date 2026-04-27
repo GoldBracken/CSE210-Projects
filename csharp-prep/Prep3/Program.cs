1aef7beec959976d0a5217244027f131e06b5c41
Austin Brackney <brackney2019@hotmail.com>
2026-04-27 14:20:07 -0600

    Modified Prep3/Program.cs Added variables, while loops, and an if-else statement that allows user to play number guessing game in the console.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        while (true) {
+        int guess = -1;
+        Random rand = new Random();
+        int magicNum = rand.Next(1,101);
+        int attempts = 0;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
+            if(guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+            attempts += 1;
+        }
+        Console.WriteLine($"You got it in {attempts} tries!");
+        Console.Write("Would you like to play again (yes/no)? ");
+        string answer = Console.ReadLine();
+        if(answer == "no")
+            {
+              break;  
+            }
+        }
     }
 }
