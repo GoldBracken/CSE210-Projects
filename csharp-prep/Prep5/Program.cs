15a6ce0158a08fd7451146197d5a9472e971ca8c
Austin Brackney <brackney2019@hotmail.com>
2026-04-30 19:23:42 -0600

    Modified Prep5/Program.cs Populated main with functions that ask for user input and displays the users inputted name, squared number, and how old they are turning this year. Created DisplayWelcome(), PromptUserName(), PromptUserNumber(), PromptUserBirthYear(out int birthYear), SquareNumber(int i), and DisplayResult(string name, int i, int birthYr) functions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,88 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+    /*
+        Writes "Welcome to the Program" to the console.
+        Parameters: none
+        returns: void
+    */
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    /*
+        Asks for users name and returns inputted string.
+        Parameters: none
+        returns: string name
+    */
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    /*
+        Asks for users favorite number and returns inputted int.
+        Parameters: none
+        returns: int num
+    */
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    /*
+        Asks for users birth year and set int x to inputted value.
+        Parameters: out int x
+        returns: void
+    */
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("Please enter your birth year: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    /*
+        Squares the int passed as a parameter.
+        Parameters: int i
+        returns: int i
+    */
+    static int SquareNumber(int i)
+    {
+        i *= i;
+        return i;
+    }
+
+    /*
+        Display the user's name, squared number, and how many years old they will turn.
+        Parameters: string name, int i, int birthYr
+        returns: void
+    */
+    static void DisplayResult(string name, int i, int birthYr)
+    {
+        Console.WriteLine($"{name}, the square of your number is {i}");
+        int age = DateTime.Now.Year - birthYr;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int birthYear;
+
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int favNum = PromptUserNumber();
+
+        PromptUserBirthYear(out birthYear);
+
+        int sqrNum = SquareNumber(favNum);
+        DisplayResult(name, sqrNum, birthYear);
     }
 }
