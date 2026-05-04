c0afd6e99be20e52f7a94f177ca30e03935d6537
Austin Brackney <brackney2019@hotmail.com>
2026-05-04 14:45:03 -0600

    Modified Program.cs Added Job.cs and Resume.cs Created Resume.cs and Job.cs public classes inside respective .cs files. Used and displayed instances of the two classes inside Program.cs file.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2023;
+        job1._endYear = 2026;
+
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "AI Engineer";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+        
+        Console.WriteLine(job2._company);
+
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "John Smith";
+        myResume._jobs = [job1, job2];
+
+        myResume.Display();
+
     }
 }
