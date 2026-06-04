012420b621a536b68421c97bd080b2cfef23829c
Austin Brackney <brackney2019@hotmail.com>
2026-06-04 17:27:50 -0600

    Modified Learning04/Program.cs Added the Assignment super class and MathAssignment and WritingAssignment child classes.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment math = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(math.GetSummary());
+
+        MathAssignment math2 = new MathAssignment("Roberto Rodriguez", "Fraction", "Section 7.3", "Problems 8-19");
+        Console.WriteLine("\n" + math2.GetSummary());
+        Console.WriteLine(math2.GetHomeworkList());
+
+        WritingAssignment paper = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine("\n" + paper.GetSummary());
+        Console.WriteLine(paper.GetWritingInformation());
     }
 }
